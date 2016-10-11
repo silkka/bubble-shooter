a463a97e475ba0ab6e2a2153161e234158d1d256
Antti Peltola <antape@utu.fi>
2016-10-11 18:06:50 +0300

    Organized the shooting mechanic

--- src/sprites/Bubble.js
+++ src/sprites/Bubble.js
@@ -2,17 +2,43 @@ import Phaser from 'phaser'
 
 export default class extends Phaser.Sprite {
 
-  constructor ({ game, x, y, asset }) {
+  constructor ({ game, x, y, asset, rightBound, leftBound}) {
     super(game, x, y, asset)
 
     this.game = game
     this.anchor.setTo(0.5)
     this.shot = false;
+    this.game.physics.arcade.enable(this);
+   
+    //Maybe something more intelligent needed with these values, for example when we have a grid object we can give it or something.
+    this.rightBound = rightBound;
+    this.leftBound = leftBound;
     
   }
 
   update () {
-      
+        //Keep the bubble bounded
+        if(this.body.right >= this.rightBound){
+            this.body.velocity.x = -this.body.velocity.x;
+        }
+
+        if(this.body.left <= this.leftBound){
+            this.body.velocity.x = -this.body.velocity.x;
+        }
+  }
+
+  shoot() {
+      if(this.shot === false){
+              this.shot = true;
+              //Get the current position of mouse and subtract the current position of the ball, you'll have the direction for the shot.
+              //Normalization is needed to ensure that the speed is constant.
+              var shotVelocity = new Phaser.Point(0,0);
+              this.game.input.activePointer.position.copyTo(shotVelocity);
+              shotVelocity.subtract(this.body.center.x,this.body.center.y);
+              shotVelocity.normalize();
+              shotVelocity.setMagnitude(500);
+              this.body.velocity = shotVelocity;
+      }
   }
 
 }
--- src/states/Game.js
+++ src/states/Game.js
@@ -9,73 +9,41 @@ export default class extends Phaser.State {
   preload () {}
 
   create () {
-//    let banner = this.add.text(this.game.world.centerX, this.game.height - 30, 'Phaser + ES6 + Webpack')
-//    banner.font = 'Nunito'
-//    banner.fontSize = 40
-//    banner.fill = '#77BFA3'
-//    banner.anchor.setTo(0.5)
-//
-//    this.mushroom = new Mushroom({
-//      game: this.game,
-//      x: this.game.world.centerX,
-//      y: this.game.world.centerY,
-//      asset: 'mushroom'
-//    })
-
-    // set the sprite width to 30% of the game width
-    //setResponsiveWidth(this.mushroom, 30, this.game.world)
-//    this.game.add.existing(this.mushroom)
-
+      //These bounds probably need something more intelligent. These are just some made up values
+      this.game.physics.startSystem(Phaser.Physics.ARCADE);
       var bubbleRadius = 32;
       var grid = {width: 20, height: 15};
       var leftBound = 50;
       var rightBound = leftBound + grid.width * bubbleRadius;
+
+      //DEBUG lines
       this.leftLine = new Phaser.Line(leftBound,0,leftBound,this.game.world.height);
       this.rightLine = new Phaser.Line(rightBound,0,rightBound,this.game.world.height);
       this.bottomLine = new Phaser.Line(leftBound,this.game.world.height - 2* bubbleRadius,rightBound,this.game.world.height - 2* bubbleRadius);
       this.centerLine = new Phaser.Line(leftBound +  (rightBound - leftBound)/2, this.game.world.height - 2* bubbleRadius, leftBound +  (rightBound - leftBound)/2, this.game.world.height);
-      this.collisionLines = [];
       
-      this.game.physics.startSystem(Phaser.Physics.ARCADE);
-      this.bubble = new Bubble({game: this.game, x :this.centerLine.start.x, y:this.centerLine.start.y+bubbleRadius/2, asset:'bluebubble'});
+      //Creating one bubble to test shooting
+      this.bubble = new Bubble({game: this.game,
+        x :this.centerLine.start.x,
+        y:this.centerLine.start.y+bubbleRadius/2,
+        asset:'bluebubble',
+        rightBound: rightBound,
+        leftBound: leftBound});
       this.game.add.existing(this.bubble);
-      this.game.physics.arcade.enable(this.bubble);
-      this.bubble.body.velocity = new Phaser.Point(0,0);
-      //this.bubble.body.velocity = new Phaser.Point(0,-10);
-      this.bubbleGrid = [];
-      //for(i = 0;i < grid.width;i++){
-      //    bubbleGrid[]
-      //}
+      
+
+
       
 
   }
   update (){
       if(this.game.input.activePointer.leftButton.isDown){
-          if(this.bubble.shot === false){
-              this.bubble.shot = true;
-              var shotVelocity = new Phaser.Point(0,0);
-              this.game.input.activePointer.position.copyTo(shotVelocity);
-              shotVelocity.subtract(this.bubble.x,this.bubble.y);
-              shotVelocity.normalize();
-              shotVelocity.setMagnitude(500);
-              this.bubble.body.velocity = shotVelocity;
-          }
+          this.bubble.shoot();
       }
-
-      if(this.bubble.body.x >= this.rightLine.start.x-32){
-          this.bubble.body.velocity.x = -this.bubble.body.velocity.x;
-      }
-
-      if(this.bubble.body.x <= this.leftLine.start.x){
-          this.bubble.body.velocity.x = -this.bubble.body.velocity.x;
-      }
-
-      
   }
 
   render () {
     if (__DEV__) {
-      //this.game.debug.spriteInfo(this.mushroom, 32, 32)
       
       this.game.debug.geom(this.leftLine,"#000000");
       this.game.debug.geom(this.bottomLine,"#000000");
